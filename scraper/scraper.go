08a482868d115420df5d8a3e0e0a10d708728413
Amin Ghasvari <amin@spcuinc.com>
2022-08-01 23:27:55 +0430

    feat: add gather all metrics, decouple parsing and scraping

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -2,7 +2,6 @@ package scraper
 
 import (
 	"fmt"
-	"github.com/SPCU/Metrics/parser"
 	"io/ioutil"
 	"net/http"
 
@@ -36,11 +35,16 @@ type Config struct {
 	Jobs   []JobConfig  `yaml:"scrape_configs"`
 }
 
+// GatherResult stores the result of fetching a service's metrics endpoint.
+type GatherResult struct {
+	JobConfig JobConfig
+	Metrics   string
+}
+
 // MetricScraper contains other applications' info so
 // that it can gather the prometheus metrics and push them to the cloud.
 type MetricScraper struct {
 	Config Config
-	Parser parser.Parser
 }
 
 // Addr returns the url of the metrics
@@ -49,30 +53,45 @@ func (jc *JobConfig) Addr() string {
 	return fmt.Sprintf("%s://%s%s", jc.Scheme, jc.StaticConfigs[0].Targets[0], jc.MetricsPath)
 }
 
-// Gather makes an HTTP request and return the string of the metrics that
+// gatherMetrics makes an HTTP request and return the string of the metrics that
 // are usually served on /metrics
-func (ms *MetricScraper) Gather(jc JobConfig) (string, error) {
+func (ms *MetricScraper) gatherMetrics(jc JobConfig) (GatherResult, error) {
 	// Http call to get the metrics
 	resp, err := http.Get(jc.Addr())
 	if err != nil {
 		log.Warn(err)
-		return "", err
+		return GatherResult{}, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Warn(err)
-		return "", err
+		return GatherResult{}, err
 	}
 
-	return string(body), nil
+	return GatherResult{JobConfig: jc, Metrics: string(body)}, nil
+}
+
+// GatherAllMetrics returns metrics of all services which are configured in YAML file.
+func (ms *MetricScraper) GatherAllMetrics() ([]GatherResult, error) {
+	var gatherResults []GatherResult
+	for _, jobConfig := range ms.Config.Jobs {
+		// Gather the metrics
+		metrics, err := ms.gatherMetrics(jobConfig)
+		if err != nil {
+			log.Warn(err)
+			continue
+		}
+		gatherResults = append(gatherResults, metrics)
+	}
+
+	return gatherResults, nil
 }
 
 // NewScraper pulls metrics from multiple sources
-func NewScraper(cfg Config, scrappingParser parser.Parser) (*MetricScraper, error) {
+func NewScraper(cfg Config) (*MetricScraper, error) {
 	return &MetricScraper{
 		Config: cfg,
-		Parser: scrappingParser,
 	}, nil
 }
 
